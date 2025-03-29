bf9cf70aa23650d93c37bcef33f15f9cede50f34
grwna <13523035@std.stei.itb.ac.id>
2025-03-29 22:59:46 +0700

    refactor: change processing for stb

--- src/header/processing.hpp
+++ src/header/processing.hpp
@@ -1,10 +1,27 @@
-#ifndef PROCESSING_HPP
+#ifndef _PROCESSING_HPP
 #define _PROCESSING_HPP
 
-#include <opencv2/opencv.hpp>
+#include "../lib/stb_image.h"
+#include "../lib/stb_image_write.h"
 #include <vector>
-#include <stdint.h>
+#include <string>
+#include <cstdint>
+#include <filesystem>
 using namespace std;
+
+/** @brief Represents an image.*/
+struct Image {
+    /* Image data. */
+    uint8_t* data;
+    /* Image width. */
+    int width;
+    /* Image height. */
+    int height;
+    /* Number of channels. */
+    int channels;
+    /* Filetype of the image */
+    string type;
+  };
 
 /**
  * @brief Read the RGB pixel data from an image file
@@ -12,35 +29,16 @@ using namespace std;
  * Note: Alpha channel values are not preserved during this step
  * 
  * @param path Full path to image file 
- * @return OpenCV Matrix Object containing RGB values from the image file
+ * @return Image struct that contains  matrix containing RGB values from the image file and dimensions
  */
-cv::Mat read_image_file(string path);
+Image read_image_file(string path);
 
 /**
- * @brief Save an OpenCV Matrix object to an image
+ * @brief Save an RGB matrix to an image
  * 
  * @param img 
  * @param path 
  */
-void save_image_file(const cv::Mat &img, string path);
-
-/**
- * @brief Turns a Matrix into a 1 dimensional vector
- * 
- * @param img An OpenCV Matrix object of an image 
- * @return Pixels of the image formatted as a 1 dimensional vector 
- */
-vector<uint8_t> matrix_to_vector(const cv::Mat& img);
-
-/**
- * @brief Turns a 1 dimensional vector into an OpenCV Matrix object
- * 
- * @param img_vec 1 dimensional vector of image's pixels
- * @param width Width of the image
- * @param height Height of the image
- * @param channels Number of channels in the image (3 for this program)
- * @return OpenCV Matrix object
- */
-cv::Mat vector_to_matrix(const vector<uint8_t> img_vec, int width, int height, int channels = 3);
+void save_image_file(const Image img, string path);
 
 #endif
--- src/processing.cpp
+++ src/processing.cpp
@@ -1,37 +1,20 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
 #include "header/processing.hpp"
 
-cv::Mat read_image_file(string path){
-    cv::Mat img = cv::imread(path, cv::IMREAD_COLOR_RGB);
+Image read_image_file(string path){
+    Image img;
+    img.data = stbi_load(path.c_str(), &img.width, &img.height, &img.channels, 3);
+    img.type = filesystem::path(path).extension().string();
     return img;
 }
 
-void save_image_file(const cv::Mat &img, string path){
-    cv::imwrite(path, img);
-}
-
-vector<uint8_t> matrix_to_vector(const cv::Mat &img){
-    int width = img.cols;
-    int height = img.rows;
-    std::vector<uint8_t> img_vec(width * height * 3); // Allocate for RGB
-
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            img_vec[(y * width + x) * 3 + 0] = img.at<cv::Vec3b>(y, x)[0]; // Red
-            img_vec[(y * width + x) * 3 + 1] = img.at<cv::Vec3b>(y, x)[1]; // Green
-            img_vec[(y * width + x) * 3 + 2] = img.at<cv::Vec3b>(y, x)[2]; // Blue
-        }
+void save_image_file(Image img, string path){
+    if (img.type == "png"){
+        stbi_write_png(path.c_str(), img.width, img.height, img.channels, img.data, img.width * img.channels);
+    } 
+    else if (img.type == "jpg" || img.type == "jpeg"){
+        stbi_write_jpg(path.c_str(), img.width, img.height, img.channels, img.data, img.width * img.channels);
     }
-    return img_vec;
-}
-
-cv::Mat vector_to_matrix(const vector<uint8_t> img_vec, int width, int height, int channels){
-    cv::Mat image(height, width, CV_8UC3); // Unsigned 8 bit, 3 Channels
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            image.at<cv::Vec3b>(y, x)[0] = img_vec[(y * width + x) * 3 + 0]; // Red
-            image.at<cv::Vec3b>(y, x)[1] = img_vec[(y * width + x) * 3 + 1]; // Green
-            image.at<cv::Vec3b>(y, x)[2] = img_vec[(y * width + x) * 3 + 2]; // Blue
-        }
-    }
-    return image;
 }
